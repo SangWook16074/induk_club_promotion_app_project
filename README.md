2367f306358ee3dd4607b4e1809375a9a61df738
HanSangWook <qazws78941@gmail.com>
2023-09-20 20:08:48 +0900

    프로젝트 생성

---
+++ README.md
@@ -0,0 +1,3 @@
+# induk_club_promotion_app_project
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/induk_club_promotion_app_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.induk_club_promotion_app_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
