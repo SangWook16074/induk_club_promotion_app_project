52bfdc32f5a968001c6b0ade0d3bb3e0eef3f324
KimGyuSeok <dwjsk2008@gmail.com>
2023-10-07 14:09:15 +0900

    로그인 완성

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:induk_club_promotion_app_project/src/view/desktop_main.dart';
-import 'package:induk_club_promotion_app_project/src/view/login.dart';
 import 'package:induk_club_promotion_app_project/src/view/mobile_main.dart';
 
 class App extends StatelessWidget {
@@ -8,14 +7,13 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Login();
-    // final currentWidth = MediaQuery.of(context).size.width;
-    //   return LayoutBuilder(builder: (context, constraints) {
-    //     if (constraints.maxWidth < 600) {
-    //       return const MobileMain();
-    //     } else {
-    //       return const DesktopMain();
-    //     }
-    //   });
+    final currentWidth = MediaQuery.of(context).size.width;
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 600) {
+        return const MobileMain();
+      } else {
+        return const DesktopMain();
+      }
+    });
   }
 }
