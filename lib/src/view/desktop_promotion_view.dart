2dff41187b151f634ac1c1c8f7cade42a59de55b
Kohminjeong <jmgo7360@gmail.com>
2023-11-08 20:02:22 +0900

    ..

--- lib/src/view/desktop_promotion_view.dart
+++ lib/src/view/desktop_promotion_view.dart
@@ -15,13 +15,7 @@ class DesktopPromotionView extends GetView<PromotionController> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: const Text(
-          'LOGO',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        title: Text('LOGO', style: Get.textTheme.displayLarge),
         elevation: 0.0,
       ),
       body: Center(
@@ -64,20 +58,17 @@ class DesktopPromotionView extends GetView<PromotionController> {
   }
 
   Widget _title() {
-    return const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
         child: Column(
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  '신규 동아리원(디자인) 추가모집',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
-                ),
+                Text('신규 동아리원(디자인) 추가모집', style: Get.textTheme.displayLarge),
               ],
             ),
-            Divider(
+            const Divider(
               color: Color(0xffE6E6E6),
             )
           ],
@@ -109,15 +100,15 @@ class DesktopPromotionView extends GetView<PromotionController> {
   }
 
   Widget _deadline() {
-    return const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
         child: Row(
           children: [
             Expanded(
               child: SizedBox(
                 child: Column(
                   children: [
-                    Row(
+                    const Row(
                       children: [
                         TitleBox(
                           label: '모집분야',
@@ -126,15 +117,11 @@ class DesktopPromotionView extends GetView<PromotionController> {
                       ],
                     ),
                     Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 20),
                       child: Row(
                         children: [
-                          Text(
-                            '백엔드/디자인',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w400),
-                          ),
+                          Text('백엔드/디자인', style: Get.textTheme.bodyLarge),
                         ],
                       ),
                     ),
@@ -146,7 +133,7 @@ class DesktopPromotionView extends GetView<PromotionController> {
               child: SizedBox(
                 child: Column(
                   children: [
-                    Row(
+                    const Row(
                       children: [
                         TitleBox(
                           label: '모집기간',
@@ -155,15 +142,12 @@ class DesktopPromotionView extends GetView<PromotionController> {
                       ],
                     ),
                     Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 20),
                       child: Row(
                         children: [
-                          Text(
-                            '2023.09.23 - 2023.09.30',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w400),
-                          ),
+                          Text('2023.09.23 - 2023.09.30',
+                              style: Get.textTheme.bodyLarge),
                         ],
                       ),
                     ),
@@ -176,21 +160,18 @@ class DesktopPromotionView extends GetView<PromotionController> {
   }
 
   Widget _object() {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
       child: Column(
         children: [
-          Row(
+          const Row(
             children: [TitleBox(label: '모집인원', fontSize: 20)],
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
             child: Row(
               children: [
-                Text(
-                  '00명',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-                ),
+                Text('00명', style: Get.textTheme.bodyLarge),
               ],
             ),
           )
@@ -200,50 +181,46 @@ class DesktopPromotionView extends GetView<PromotionController> {
   }
 
   Widget _info() {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 40),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
       child: Column(
         children: [
-          Row(
+          const Row(
             children: [TitleBox(label: '활동내용', fontSize: 20)],
           ),
           Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
               child: Text(
-                '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
-                '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
-                '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
-                'UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.'
-                'UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.'
-                '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
-                'UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.'
-                '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
-                'UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.'
-                '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
-                'UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-              ))
+                  '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
+                  '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
+                  '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
+                  'UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.'
+                  'UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.'
+                  '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
+                  'UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.'
+                  '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
+                  'UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.'
+                  '2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.'
+                  'UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.',
+                  style: Get.textTheme.bodyLarge))
         ],
       ),
     );
   }
 
   Widget _target() {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
       child: Column(
         children: [
-          Row(
+          const Row(
             children: [TitleBox(label: '우대학과', fontSize: 20)],
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
             child: Row(
               children: [
-                Text(
-                  '학과 무관',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-                ),
+                Text('학과 무관', style: Get.textTheme.bodyLarge),
               ],
             ),
           )
@@ -253,21 +230,19 @@ class DesktopPromotionView extends GetView<PromotionController> {
   }
 
   Widget _period() {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
       child: Column(
         children: [
-          Row(
+          const Row(
             children: [TitleBox(label: '활동기간', fontSize: 20)],
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
             child: Row(
               children: [
-                Text(
-                  '2023년 3월 10일 - 2024년 1월 10일',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-                ),
+                Text('2023년 3월 10일 - 2024년 1월 10일',
+                    style: Get.textTheme.bodyLarge),
               ],
             ),
           )
@@ -278,120 +253,110 @@ class DesktopPromotionView extends GetView<PromotionController> {
 
   Widget _infoBox() {
     return Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-        child: ClipRRect(
-            borderRadius: BorderRadius.circular(16.0),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
-              child: Container(
-                padding: const EdgeInsets.all(20.0),
-                width: 400,
-                height: 300,
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        Color(0xffE6E6E6),
-                        Color(0xffE6E6E6),
-                      ]),
-                  borderRadius: BorderRadius.circular(8.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.7),
-                      spreadRadius: 0,
-                      blurRadius: 5.0,
-                      offset:
-                          const Offset(0, 200), // changes position of shadow
-                    ),
-                  ],
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16.0),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xffE6E6E6),
+                    Color(0xffE6E6E6),
+                  ]),
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.7),
+                  spreadRadius: 0,
+                  blurRadius: 5.0,
+                  offset: const Offset(0, 200), // changes position of shadow
                 ),
-                alignment: Alignment.center,
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ],
+            ),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
                   children: [
-                    Column(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'A&I',
-                              style:
-                                  TextStyle(fontSize: 25, color: Colors.black),
-                            ),
-                            Icon(
-                              Icons.more_vert,
-                              color: Color(0xff713eff),
-                            )
-                          ],
+                        Text(
+                          'A&I',
+                          style: TextStyle(fontSize: 25, color: Colors.black),
                         ),
-                        Divider(
+                        Icon(
+                          Icons.more_vert,
                           color: Color(0xff713eff),
-                        ),
+                        )
                       ],
                     ),
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              '동아리 개설일',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w700),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              '2023년 1월 10일',
-                              style: TextStyle(color: Colors.black),
-                            )
-                          ],
-                        ),
-                      ],
+                    Divider(
+                      color: Color(0xff713eff),
                     ),
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              '동아리 분류',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w700),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              '교내동아리',
-                              style: TextStyle(color: Colors.black),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                    Row(children: [
-                      Expanded(
-                        child: SignButton(
-                          width: double.infinity,
-                          height: 45,
-                          child: Text(
-                            '지원하기',
-                            style: TextStyle(
-                                color: Color(0xffffffff),
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                    ]),
                   ],
                 ),
-              ),
-            )));
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('동아리 개설일', style: Get.textTheme.displayMedium)
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('2023년 1월 10일', style: Get.textTheme.bodyMedium)
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          '동아리 분류',
+                          style: Get.textTheme.displayMedium,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          '교내동아리',
+                          style: Get.textTheme.bodyMedium,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                const Row(children: [
+                  Expanded(
+                    child: SignButton(
+                      width: double.infinity,
+                      height: 45,
+                      child: Text(
+                        '지원하기',
+                        style: TextStyle(
+                            color: Color(0xffffffff),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ]),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
