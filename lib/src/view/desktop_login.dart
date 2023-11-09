38dd04c702555e8e351110153fe8b76e42510b18
KimGyuSeok <dwjsk2008@gmail.com>
2023-11-09 20:32:43 +0900

    desktop login 시작

--- lib/src/view/desktop_login.dart
+++ lib/src/view/desktop_login.dart
@@ -1,87 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:induk_club_promotion_app_project/src/widget/complete_box.dart';
+import 'package:get/get.dart';
+import 'package:induk_club_promotion_app_project/src/controllers/login_controller.dart';
+import 'package:induk_club_promotion_app_project/src/widget/account_box.dart';
+import 'package:induk_club_promotion_app_project/src/widget/login_box.dart';
 import 'package:induk_club_promotion_app_project/src/widget/terms_check_box.dart';
 
-class DesktopLogin extends StatefulWidget {
+class DesktopLogin extends GetView<LoginController> {
   const DesktopLogin({super.key});
 
   @override
-  State<DesktopLogin> createState() => _DesktopLoginState();
-}
-
-class _DesktopLoginState extends State<DesktopLogin> {
-  final TextEditingController id = TextEditingController();
-  final TextEditingController password = TextEditingController();
-  late int pageIndex = 0;
-
-  @override
-  void dispose() {
-    super.dispose();
-    id.dispose();
-    password.dispose();
-  }
-
-  void moveToSecond() {
-    setState(() {
-      id.clear();
-      password.clear();
-      pageIndex++;
-    });
-  }
-
-  void moveToNext() => setState(() {
-        pageIndex++;
-      });
-
-  void resetIndex() => setState(() {
-        pageIndex = 0;
-      });
-
-  @override
   Widget build(BuildContext context) {
-    List<Widget> boxes = [
-      // LoginBox(
-      //   lenght: 400,
-      //   id: id,
-      //   password: password,
-      //   moveToFindAccount: () {},
-      //   moveToFindPassword: () {},
-      //   moveToSignUp: moveToSecond,
-      // ),
-      TermsCheckBox(
-        moveToNext: moveToNext,
-      ),
-      // AccountBox(
-      //   lenght: 400,
-      //   id: id,
-      //   password: password,
-      //   moveToNext: moveToNext,
-      // ),
-      CompleteBox(
-        lenght: 400,
-        moveToLogin: resetIndex,
-      )
-    ];
-
     return Scaffold(
-      backgroundColor: const Color(0xff191123),
-      body: Container(
-          padding: const EdgeInsets.all(30.0),
-          alignment: Alignment.center,
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                Colors.white,
-                Color(0xffede1fa),
-                Color(0xffe1c7ff),
-                Color(0xffdebdff),
-                Color(0xffbd7efc),
-                Color(0xff8f00ff),
-                // Color(0xff1e1e1e),
-              ])),
-          child: boxes[pageIndex]),
-    );
+        body: Center(
+      child: Container(
+        decoration: BoxDecoration(border: Border.all()),
+        width: Get.size.height * 0.8,
+        height: Get.size.height * 0.8,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _logo(),
+            _body(),
+          ],
+        ),
+      ),
+    ));
   }
+
+  Widget _logo() => const SizedBox(
+      height: 80,
+      child: Text(
+        "Logo",
+        style: TextStyle(fontSize: 40),
+      ));
+
+  Widget _body() => SizedBox(
+        height: Get.size.height * 0.4,
+        child: Obx(
+          () => IndexedStack(index: controller.index, children: [
+            LoginBox(
+              email: controller.emailController,
+              password: controller.passwordController,
+            ),
+            const TermsCheckBox(),
+            const AccountBox()
+          ]),
+        ),
+      );
 }
