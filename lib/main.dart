1dc76ea2f1aa97970be9ed29d6cacc1d28478d69
KimGyuSeok <dwjsk2008@gmail.com>
2023-11-13 18:55:19 +0900

    목업 json 수정

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:induk_club_promotion_app_project/src/app.dart';
 import 'package:induk_club_promotion_app_project/src/bindings/init_binding.dart';
-import 'package:induk_club_promotion_app_project/src/data/model/promotion.dart';
-import 'package:induk_club_promotion_app_project/src/view/desktop_my_page.dart';
-import 'package:induk_club_promotion_app_project/src/view/desktop_promotion_view.dart';
 
 void main() {
   runApp(const MainApp());
