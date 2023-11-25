bdbf2549a79728aab9e85cb7a3081fccd6d48559
minjeongKoh <jmgo7360@gmail.com>
2023-11-25 18:17:29 +0900

    마이페이지 동아리소개랑 내가 쓴 글 수정 완료

--- lib/src/view/desktop_my_page.dart
+++ lib/src/view/desktop_my_page.dart
@@ -15,6 +15,7 @@ class DesktopMyPage extends StatelessWidget {
         _appBar(),
         _myInfo(),
         _clubInfo(),
+        _myPromotions(),
       ],
     ));
   }
@@ -63,135 +64,189 @@ class DesktopMyPage extends StatelessWidget {
                 type: ProfileType.MYPAGE,
               ),
             ),
+            const Positioned(
+                top: 100,
+                left: 450,
+                bottom: 10,
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                '마이페이지',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              '담딩자: 어쩌고',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ))
           ],
         ),
       ),
     );
   }
 
-  Widget _clubInfo() => SliverToBoxAdapter(
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 100.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  Widget _clubInfo() {
+    return SliverToBoxAdapter(
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 300.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TitleBox(
+                  label: '동아리 소개',
+                  fontSize: 20,
+                ),
+                Text(
+                  '수정',
+                  style: TextStyle(fontSize: 20, color: Colors.grey),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 300.0),
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                  color: const Color(0xffe0e0e0),
+                  border: Border.all(color: const Color(0xffb5b5b5)),
+                  borderRadius: BorderRadius.circular(8.0)),
+              child: Column(
                 children: [
-                  TitleBox(
-                    label: '동아리 소개',
-                    fontSize: 20,
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 5,
+                          child: Row(
+                            children: [
+                              Text(
+                                '동아리명',
+                                style: Get.theme.textTheme.displayMedium,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 5,
+                          child: Row(
+                            children: [
+                              Text(
+                                'A&I',
+                                style: Get.theme.textTheme.displayMedium,
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  Text(
-                    '수정',
-                    style: TextStyle(fontSize: 20, color: Colors.grey),
-                  )
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 5,
+                          child: Row(
+                            children: [
+                              Text(
+                                '동아리개설일',
+                                style: Get.theme.textTheme.displayMedium,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 5,
+                          child: Row(
+                            children: [
+                              Text('2023년 1월 10일',
+                                  style: Get.theme.textTheme.displayMedium)
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 5,
+                          child: Row(
+                            children: [
+                              Text(
+                                '동아리분류',
+                                style: Get.theme.textTheme.displayMedium,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 5,
+                          child: Row(
+                            children: [
+                              Text(
+                                '교내스터디',
+                                style: Get.theme.textTheme.displayMedium,
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              child: Container(
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                    color: const Color(0xffe0e0e0),
-                    border: Border.all(color: const Color(0xffb5b5b5)),
-                    borderRadius: BorderRadius.circular(8.0)),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            flex: 5,
-                            child: Row(
-                              children: [
-                                Text(
-                                  '동아리명',
-                                  style: Get.theme.textTheme.displayMedium,
-                                ),
-                              ],
-                            ),
-                          ),
-                          Expanded(
-                            flex: 5,
-                            child: Row(
-                              children: [
-                                Text(
-                                  'A&I',
-                                  style: Get.theme.textTheme.displayMedium,
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            flex: 5,
-                            child: Row(
-                              children: [
-                                Text(
-                                  '동아리개설일',
-                                  style: Get.theme.textTheme.displayMedium,
-                                ),
-                              ],
-                            ),
-                          ),
-                          Expanded(
-                            flex: 5,
-                            child: Row(
-                              children: [
-                                Text('2023년 1월 10일',
-                                    style: Get.theme.textTheme.displayMedium)
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            flex: 5,
-                            child: Row(
-                              children: [
-                                Text(
-                                  '동아리분류',
-                                  style: Get.theme.textTheme.displayMedium,
-                                ),
-                              ],
-                            ),
-                          ),
-                          Expanded(
-                            flex: 5,
-                            child: Row(
-                              children: [
-                                Text(
-                                  '교내스터디',
-                                  style: Get.theme.textTheme.displayMedium,
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
+          )
+        ],
+      ),
+    );
+  }
+}
+
+Widget _myPromotions() {
+  return const SliverToBoxAdapter(
+    child: Column(children: [
+      Padding(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 300.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TitleBox(label: '내가 쓴 글', fontSize: 20),
           ],
         ),
-      );
+      ),
+      Text('내가 쓴 글이 없습니다.')
+    ]),
+  );
 }
