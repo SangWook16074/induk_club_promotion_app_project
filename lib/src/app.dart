83a016d60a9eabb615594b73a41f4972afe13934
HanSangWook <qazws78941@gmail.com>
2023-11-27 19:35:10 +0900

    app.dart에서 지정하는 라우팅 교체

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:induk_club_promotion_app_project/src/controllers/app_controller.dart';
-import 'package:induk_club_promotion_app_project/src/responsible_layout.dart';
-import 'package:induk_club_promotion_app_project/src/view/desktop_main.dart';
-import 'package:induk_club_promotion_app_project/src/view/mobile_app.dart';
-import 'package:induk_club_promotion_app_project/src/view/tablet_main.dart';
+import 'package:induk_club_promotion_app_project/src/view/home_screen.dart';
 
 class App extends GetView<AppController> {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: ResponsibleLayout(
-        mobile: MobileApp(),
-        tablet: TabletMain(),
-        desktop: DesktopMain(),
-      ),
-    );
+    return const Scaffold(body: HomeScreen());
   }
 }
