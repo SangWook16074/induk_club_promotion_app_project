a6986cc88875934e79b74878aa46ae0622942356
HanSangWook <qazws78941@gmail.com>
2023-12-01 19:31:47 +0900

    패키지명 변경

--- lib/src/widget/apple_login_button.dart
+++ lib/src/widget/apple_login_button.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:induk_club_promotion_app_project/src/widget/social_image_icon.dart';
 
 class AppleLoginButton extends StatelessWidget {
-  const AppleLoginButton({super.key});
+  final void Function()? onTap;
+  const AppleLoginButton({super.key, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5.0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4.0)),
-      child: Container(
-        width: 50,
-        height: 50,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            color: Colors.black, borderRadius: BorderRadius.circular(4.0)),
-        child: ClipRRect(
-            borderRadius: BorderRadius.circular(4.0),
-            child: SocialImageIcon(path: SocialImagePath.apple)),
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4.0)),
+        child: Container(
+          width: 50,
+          height: 50,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.black, borderRadius: BorderRadius.circular(4.0)),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(4.0),
+              child: SocialImageIcon(path: SocialImagePath.apple)),
+        ),
       ),
     );
   }
--- lib/src/widget/google_login_button.dart
+++ lib/src/widget/google_login_button.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:induk_club_promotion_app_project/src/widget/social_image_icon.dart';
 
 class GoogleLoginButton extends StatelessWidget {
-  const GoogleLoginButton({super.key});
+  final void Function()? onTap;
+  const GoogleLoginButton({super.key, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5.0,
-      child: Container(
-        padding: const EdgeInsets.all(8.0),
-        width: 50,
-        height: 50,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(4.0)),
-        child: ClipRRect(
-            borderRadius: BorderRadius.circular(4.0),
-            child: SocialImageIcon(path: SocialImagePath.google)),
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        elevation: 5.0,
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          width: 50,
+          height: 50,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(4.0)),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(4.0),
+              child: SocialImageIcon(path: SocialImagePath.google)),
+        ),
       ),
     );
   }
