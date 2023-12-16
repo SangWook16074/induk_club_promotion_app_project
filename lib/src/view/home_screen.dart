b49e41716905becaf7a7e5fef236deab4a290127
HanSangWook <qazws78941@gmail.com>
2023-12-16 14:30:24 +0900

    홍보글 상세 페이지 반응형 작업 완료

--- lib/src/view/home_screen.dart
+++ lib/src/view/home_screen.dart
@@ -8,6 +8,7 @@ import 'package:induk_club_promotion_app_project/src/responsible_layout.dart';
 import 'package:induk_club_promotion_app_project/src/view/desktop_promotion_view.dart';
 import 'package:induk_club_promotion_app_project/src/view/login_screen.dart';
 import 'package:induk_club_promotion_app_project/src/view/mobile_promotion_view.dart';
+import 'package:induk_club_promotion_app_project/src/view/promotion_screen.dart';
 import 'package:induk_club_promotion_app_project/src/view/tablet_promotion_view.dart';
 import 'package:induk_club_promotion_app_project/src/widget/promotion_item.dart';
 import 'package:induk_club_promotion_app_project/src/widget/search_text_field.dart';
@@ -227,15 +228,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }) =>
       GestureDetector(
           onTap: () {
-            Get.to(
-              () => ResponsibleLayout(
-                mobile: const MobilePromotionView(),
-                tablet: const TabletPromotionView(),
-                desktop: DesktopPromotionView(
-                  promotion: promotion,
-                ),
-              ),
-            );
+            Get.to(() => PromotionScreen(promotion: promotion));
           },
           child: PromotionItem(
             date: "D - 9",
--- lib/src/view/promotion_screen.dart
+++ lib/src/view/promotion_screen.dart
@@ -1,15 +1,365 @@
+import 'dart:ui';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:induk_club_promotion_app_project/src/responsible_layout.dart';
+import 'package:induk_club_promotion_app_project/src/widget/sign_button.dart';
+import 'package:induk_club_promotion_app_project/src/widget/title_box.dart';
+import '../data/model/promotion.dart';
 
 class PromotionScreen extends StatefulWidget {
-  const PromotionScreen({super.key});
+  final Promotion promotion;
+  const PromotionScreen({super.key, required this.promotion});
 
   @override
   State<PromotionScreen> createState() => _PromotionScreenState();
 }
 
 class _PromotionScreenState extends State<PromotionScreen> {
+  final EdgeInsetsGeometry _mobilePadding =
+      const EdgeInsets.symmetric(horizontal: 10, vertical: 5);
+  final EdgeInsetsGeometry _desktopPadding =
+      const EdgeInsets.symmetric(horizontal: 40, vertical: 20);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('LOGO', style: Get.textTheme.displayLarge),
+        elevation: 0.0,
+      ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: (ResponsibleLayout.isDesktop(context))
+                  ? const EdgeInsets.only(left: 200, right: 400)
+                  : const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  _img(),
+                  _title(),
+                  _closeAt(),
+                  _beginToEnd(),
+                  _object(),
+                  _content(),
+                  _contentOfAcivity(),
+                ],
+              ),
+            ),
+          ),
+          (ResponsibleLayout.isDesktop(context))
+              ? Positioned(right: 100, child: _infoBox())
+              : Align(alignment: Alignment.bottomCenter, child: _join())
+        ],
+      ),
+    );
+  }
+
+  Widget _title() {
+    return Padding(
+        padding: (ResponsibleLayout.isDesktop(context))
+            ? _desktopPadding
+            : _mobilePadding,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('신규 동아리원(디자인) 추가모집', style: Get.textTheme.displayLarge),
+              ],
+            ),
+            const Divider(
+              color: Color(0xffE6E6E6),
+            )
+          ],
+        ));
+  }
+
+  Widget _img() {
+    return Padding(
+      padding: (ResponsibleLayout.isDesktop(context))
+          ? const EdgeInsets.symmetric(
+              horizontal: 40,
+            )
+          : const EdgeInsets.all(8.0),
+      child: SizedBox(
+        child: CarouselSlider.builder(
+            itemCount: 3,
+            itemBuilder: (context, index, realIndex) => Container(
+                  height: 400,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  color: const Color(0xffE6E6E6),
+                  child: Text('이미지 ${index + 1}'),
+                ),
+            options: CarouselOptions(
+              viewportFraction: 1,
+              aspectRatio: 1.5,
+              autoPlay: true,
+            )),
+      ),
+    );
+  }
+
+  Widget _closeAt() {
+    return Padding(
+        padding: (ResponsibleLayout.isDesktop(context))
+            ? _desktopPadding
+            : _mobilePadding,
+        child: SizedBox(
+          child: Column(
+            children: [
+              const Row(
+                children: [
+                  TitleBox(
+                    label: '모집마감',
+                    fontSize: 20,
+                  )
+                ],
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child: Row(
+                  children: [
+                    Text(widget.promotion.closeAt.toString(),
+                        style: Get.textTheme.bodyMedium),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  Widget _object() {
+    return Padding(
+      padding: (ResponsibleLayout.isDesktop(context))
+          ? _desktopPadding
+          : _mobilePadding,
+      child: Column(
+        children: [
+          const Row(
+            children: [TitleBox(label: '모집인원', fontSize: 20)],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Row(
+              children: [
+                Text('${widget.promotion.requiredPeople}명',
+                    style: Get.textTheme.bodyMedium),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _content() {
+    return Padding(
+      padding: (ResponsibleLayout.isDesktop(context))
+          ? _desktopPadding
+          : _mobilePadding,
+      child: Column(
+        children: [
+          const Row(
+            children: [TitleBox(label: '동아리소개', fontSize: 20)],
+          ),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: Text(widget.promotion.content,
+                  style: Get.textTheme.bodyMedium))
+        ],
+      ),
+    );
+  }
+
+  Widget _contentOfAcivity() {
+    return Padding(
+      padding: (ResponsibleLayout.isDesktop(context))
+          ? _desktopPadding
+          : _mobilePadding,
+      child: Column(
+        children: [
+          const Row(
+            children: [TitleBox(label: '활동내용', fontSize: 20)],
+          ),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: Text(widget.promotion.contentOfActivity,
+                  style: Get.textTheme.bodyMedium))
+        ],
+      ),
+    );
+  }
+
+  Widget _beginToEnd() {
+    return Padding(
+      padding: (ResponsibleLayout.isDesktop(context))
+          ? _desktopPadding
+          : _mobilePadding,
+      child: Column(
+        children: [
+          const Row(
+            children: [TitleBox(label: '활동기간', fontSize: 20)],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Row(
+              children: [
+                Text(
+                  '${widget.promotion.begin}\n${widget.promotion.end}',
+                  style: Get.textTheme.bodyMedium,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _infoBox() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16.0),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xffE6E6E6),
+                    Color(0xffE6E6E6),
+                  ]),
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.7),
+                  spreadRadius: 0,
+                  blurRadius: 5.0,
+                  offset: const Offset(0, 200), // changes position of shadow
+                ),
+              ],
+            ),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'A&I',
+                          style: TextStyle(fontSize: 25, color: Colors.black),
+                        ),
+                        Icon(
+                          Icons.more_vert,
+                          color: Color(0xff713eff),
+                        )
+                      ],
+                    ),
+                    Divider(
+                      color: Color(0xff713eff),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('동아리 개설일', style: Get.textTheme.headlineMedium)
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('2023년 1월 10일', style: Get.textTheme.bodyMedium)
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          '동아리 분류',
+                          style: Get.textTheme.headlineMedium,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          '교내동아리',
+                          style: Get.textTheme.bodyMedium,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                const Row(children: [
+                  Expanded(
+                    child: SignButton(
+                      width: double.infinity,
+                      height: 45,
+                      child: Text(
+                        '지원하기',
+                        style: TextStyle(
+                            color: Color(0xffffffff),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ]),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _join() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+      height: 80,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+            ...List.generate(
+                10, (index) => Colors.white.withOpacity(index * 0.1)),
+            ...List.generate(10, (index) => Colors.white)
+          ])),
+      child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff713eff)),
+              onPressed: () {},
+              child: const Text(
+                "지원하기",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+              ))),
+    );
   }
 }
