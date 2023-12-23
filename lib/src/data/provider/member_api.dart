1d034674d372c76c22b70bdf281782ba54c0ca0b
HanSangWook <qazws78941@gmail.com>
2023-12-23 15:41:40 +0900

    회원가입 기능 관련 리소스 private 설정

--- lib/src/data/provider/member_api.dart
+++ lib/src/data/provider/member_api.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:induk_club_promotion_app_project/src/constants/url.dart';
 
 class MemberApi {
   final Dio dio;
@@ -8,9 +9,7 @@ class MemberApi {
 
   Future<String> signUp(Map<String, dynamic> data) {
     try {
-      return dio
-          .post("http://localhost:8080/api/member/signup", data: data)
-          .then((resp) {
+      return dio.post(Url.signUpUrl, data: data).then((resp) {
         if (resp.statusCode == 200) {
           return resp.data;
         } else {
