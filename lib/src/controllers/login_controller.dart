74bc957043688b3b0f6fb9cf01213bf88fb2db97
minjeongKoh <jmgo7360@gmail.com>
2024-01-17 20:28:38 +0900

    소셜로그인 제거버전 pull

--- lib/src/controllers/login_controller.dart
+++ lib/src/controllers/login_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import 'package:get/get.dart';
 import 'package:induk_club_promotion_app_project/src/bindings/resister_binding.dart';
 import 'package:induk_club_promotion_app_project/src/data/model/member.dart';
