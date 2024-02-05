91ad83fe417b35c7c20e1632d37a3ef5739ca6c5
minjeongKoh <jmgo7360@gmail.com>
2024-02-05 21:21:33 +0900

    conflict 해결

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class MainApp extends StatelessWidget {
                   fontWeight: FontWeight.w600)),
           appBarTheme: const AppBarTheme(backgroundColor: Color(0xff9933ff)),
           scaffoldBackgroundColor: const Color(0xffffffff)),
-      home: const LoginScreen(),
+      home: const App(),
       initialBinding: InitBinding(),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
