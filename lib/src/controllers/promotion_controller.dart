3b83299ce9a820d0dfaf9cda8b179feae43cfa7e
HanSangWook <qazws78941@gmail.com>
2024-02-05 20:28:27 +0900

    메인화면 서버 연결 예외처리 수정

--- lib/src/controllers/promotion_controller.dart
+++ lib/src/controllers/promotion_controller.dart
@@ -1,18 +1,21 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:induk_club_promotion_app_project/src/app.dart';
-import 'package:induk_club_promotion_app_project/src/controllers/status_controller.dart';
+import 'package:induk_club_promotion_app_project/src/constants/status.dart';
 import 'package:induk_club_promotion_app_project/src/data/repository/promotion_repository.dart';
 
 import '../data/model/promotion.dart';
 
 class PromotionController extends GetxController {
   final Rx<List<Promotion>> _promotions = Rx<List<Promotion>>([]);
+  final Rx<Status> _status = Rx<Status>(Status.LOADING);
   final PromotionRepository promotionRepository;
   final FlutterSecureStorage storage = const FlutterSecureStorage();
   PromotionController({required this.promotionRepository});
 
+  PromotionController get to => Get.find();
   List<Promotion> get promotions => _promotions.value;
+  Status get status => _status.value;
   @override
   void onReady() {
     super.onReady();
@@ -21,8 +24,15 @@ class PromotionController extends GetxController {
 
   /// 홍보글 새로고침 메소드
   Future<void> fetchData() async {
-    final promotions = await promotionRepository.getPromotions();
-    _promotions(promotions);
+    try {
+      // 앱 상태 변경
+      _status(Status.LOADING);
+      final promotions = await promotionRepository.getPromotions();
+      _promotions(promotions);
+      _status(Status.LOADED);
+    } on Exception {
+      _status(Status.ERROR);
+    }
   }
 
   // 홍보글 생성 메소드
