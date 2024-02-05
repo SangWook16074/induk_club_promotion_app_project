553df3c89d9b5184acb2eb63d8d12c7e1be830e5
HanSangWook <qazws78941@gmail.com>
2024-02-05 21:31:29 +0900

    회원가입 화면 수정 완료

--- lib/src/view/login_screen.dart
+++ lib/src/view/login_screen.dart
@@ -187,23 +187,31 @@ class _LoginScreenState extends State<LoginScreen> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        GestureDetector(
-            onTap: controller.moveToFindPassword,
-            child: Text(
-              '비밀번호 찾기',
-              style: Get.theme.textTheme.bodySmall,
-            )),
+        Expanded(
+          child: InkWell(
+              onTap: controller.moveToFindPassword,
+              child: Center(
+                child: Text(
+                  '비밀번호 찾기',
+                  style: Get.theme.textTheme.bodySmall,
+                ),
+              )),
+        ),
         Container(
           width: 2,
           height: 12,
           color: Colors.grey,
         ),
-        InkWell(
-            onTap: controller.moveToResister,
-            child: Text(
-              '회원가입',
-              style: Get.theme.textTheme.bodySmall,
-            )),
+        Expanded(
+          child: InkWell(
+              onTap: controller.moveToResister,
+              child: Center(
+                child: Text(
+                  '회원가입',
+                  style: Get.theme.textTheme.bodySmall,
+                ),
+              )),
+        ),
       ],
     );
   }
