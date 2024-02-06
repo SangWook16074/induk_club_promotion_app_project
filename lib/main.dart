63040b969d990c2d575ec11daab089419ce01bb1
minjeongKoh <jmgo7360@gmail.com>
2024-02-06 17:31:43 +0900

    수정

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class MainApp extends StatelessWidget {
                   fontWeight: FontWeight.w600)),
           appBarTheme: const AppBarTheme(backgroundColor: Color(0xff9933ff)),
           scaffoldBackgroundColor: const Color(0xffffffff)),
-      home: const ProfileEditScreen(),
+      home: const App(),
       initialBinding: InitBinding(),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
